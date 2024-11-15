058589b9d1c19cda2ea89225b13b7a43dc270718
Adrian Helbig <adrian_helbig@outlook.de>
2024-11-15 16:05:24 +0100

    refactor: Simplify setup_python function and rename parameters
    - Refactored the `setup_python` function to consolidate common steps for installation and update processes.
    - Moved common logic into a new `download_and_install_python` function to reduce redundancy.
    - Renamed parameters `REQUIRED_PYTHON_VERSION` and `REQUIRED_PYTHON_VERSION_SUFFIX` to `MIN_REQUIRED_PYTHON_VERSION` and `MIN_REQUIRED_PYTHON_VERSION_SUFFIX` for clarity.

--- Scripts/Win/build_script.sh
+++ Scripts/Win/build_script.sh
@@ -3,14 +3,14 @@
 source Helpers/utils.sh
 source Helpers/setup_python.sh
 
-REQUIRED_PYTHON_VERSION="3.14.0"
-REQUIRED_PYTHON_VERSION_SUFFIX="a1"
+MIN_REQUIRED_PYTHON_VERSION="3.14.0"
+MIN_REQUIRED_PYTHON_VERSION_SUFFIX="a1"
 
 # Ensure the script is running as admin
 check_admin
 
 # Call the function to install or update Python
-setup_python $REQUIRED_PYTHON_VERSION $REQUIRED_PYTHON_VERSION_SUFFIX
+setup_python $MIN_REQUIRED_PYTHON_VERSION $MIN_REQUIRED_PYTHON_VERSION_SUFFIX
 if [ $? -ne 0 ]; then
     echo "An error occurred during the setup of Python."
 fi
