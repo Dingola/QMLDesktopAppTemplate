2f2a0aa4cfbb64e585a1dcc38c5c9ee781f0a0ef
Adrian Helbig <adrian_helbig@outlook.de>
2024-11-19 11:01:00 +0100

    feat: Add NonCopyable, NonMoveable, and Singleton classes
    - Introduced NonCopyable to prevent copying of derived classes.
    - Introduced NonMoveable to prevent moving of derived classes.
    - Introduced Singleton using CRTP, inheriting from NonCopyable and NonMoveable.
    - Added documentation for all classes.

---
+++ QMLDesktopAppTemplate/Headers/Common/NonCopyable.h
@@ -0,0 +1,53 @@
+#pragma once
+
+/** @file
+ *  @brief This file contains the definition of the NonCopyable class.
+ */
+
+namespace QmlApp
+{
+	/**
+	 * @class NonCopyable
+	 * @brief A base class to prevent copying of derived classes.
+	 *
+	 * This class is used to prevent copying of derived classes by deleting
+	 * the copy constructor and copy assignment operator.
+	 */
+	class NonCopyable
+	{
+	protected:
+		/**
+		 * @brief Constructs a NonCopyable object.
+		 *
+		 * The constructor is protected to allow derived classes to construct the object.
+		 */
+		NonCopyable() = default;
+
+		/**
+		 * @brief Destroys the NonCopyable object.
+		 *
+		 * The destructor is virtual to ensure proper cleanup of derived classes.
+		 */
+		virtual ~NonCopyable() = default;
+
+	public:
+		/**
+		 * @brief Deleted copy constructor.
+		 *
+		 * Prevents copying of the object.
+		 *
+		 * @param other The object to copy from.
+		 */
+		NonCopyable(const NonCopyable&) = delete;
+
+		/**
+		 * @brief Deleted copy assignment operator.
+		 *
+		 * Prevents copying of the object.
+		 *
+		 * @param other The object to copy from.
+		 * @return A reference to this object.
+		 */
+		NonCopyable& operator= (const NonCopyable&) = delete;
+	};
+} // namespace QmlApp
---
+++ QMLDesktopAppTemplate/Headers/Common/Singleton.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include "NonCopyable.h"
+#include "NonMoveable.h"
+
+/** @file
+ *  @brief This file contains the definition of the Singleton class.
+ */
+
+namespace QmlApp
+{
+	/**
+	 * @class Singleton
+	 * @brief Meyers Singleton Pattern using CRTP
+	 *
+	 * This class implements the Meyers Singleton Pattern using the Curiously Recurring Template Pattern (CRTP).
+	 * Note: Derived classes need to declare 'friend class Singleton<T>'.
+	 *       Derived classes must also have their constructor set to private to prevent instantiation.
+	 */
+	template<typename T>
+	class Singleton : public NonCopyable, public NonMoveable
+	{
+	protected:
+		/**
+		 * @brief Constructs a Singleton object.
+		 *
+		 * The constructor is protected to allow derived classes to construct the object.
+		 */
+		Singleton() = default;
+
+		/**
+		 * @brief Destroys the Singleton object.
+		 *
+		 * The destructor is virtual to ensure proper cleanup of derived classes.
+		 */
+		virtual ~Singleton() = default;
+
+	public:
+		/**
+		 * @brief Returns the singleton instance of the class.
+		 *
+		 * This function returns the singleton instance of the class, creating it if it does not already exist.
+		 * The instance is created in a thread-safe manner.
+		 *
+		 * @return The singleton instance of the class.
+		 */
+		static T& get_instance()
+		{
+			static T instance;  // Thread-safe in C++11 and later
+			return instance;
+		}
+	};
+} // namespace QmlApp
