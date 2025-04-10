6208c34d72cb86854b66934336592c24d34de618
Adrian Helbig <adrian_helbig@outlook.de>
2025-04-10 18:15:33 +0200

    Move config files for clang-tidy, clang-format and Doxygen to a "Configs" folder within the Solution directory.
    - Update CMake files to locate these configs correctly.
    - Change naming convention for the CMake Doxygen option.
    - Adjust the doc target names accordingly.

--- QML_Project/ThirdParty/Doxygen.cmake
+++ QML_Project/ThirdParty/Doxygen.cmake
@@ -1,20 +1,20 @@
-set(build_doc ${doc_sub_target_name}_BUILD_DOC)
-option(${build_doc} "Build documentation (${doc_sub_target_name})" OFF)
+set(BUILD_DOC ${DOC_OPTION_NAME}_BUILD_DOC)
+option(${BUILD_DOC} "Build documentation (${DOC_OPTION_NAME})" OFF)
 
-if (${build_doc})
+if (${BUILD_DOC})
 	find_package(Doxygen)
 
 	if (DOXYGEN_FOUND)
 		# set input and output files
-		set(DOXYGEN_IN ${CMAKE_SOURCE_DIR}/Doxyfile.in)
+		set(DOXYGEN_IN ${CMAKE_SOURCE_DIR}/Configs/Doxyfile.in)
 		set(DOXYGEN_OUT ${CMAKE_CURRENT_SOURCE_DIR}/Docs/Doxyfile)
 
 		# request to configure the file
 		configure_file(${DOXYGEN_IN} ${DOXYGEN_OUT} @ONLY)
-		message("Doxygen build started for ${doc_sub_target_name}")
+		message("Doxygen build started for ${DOC_TARGET_NAME}")
 
 		# note the option ALL which allows to build the docs together with the application
-		add_custom_target(Doc_Doxygen_${doc_sub_target_name} ALL
+		add_custom_target(_run_doxygen_${DOC_TARGET_NAME} ALL
 			COMMAND ${DOXYGEN_EXECUTABLE} ${DOXYGEN_OUT}
 			WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
 			COMMENT "Generating API documentation with Doxygen"
@@ -23,4 +23,4 @@ if (${build_doc})
 	  message("Doxygen need to be installed to generate the doxygen documentation")
 	endif(DOXYGEN_FOUND)
 	
-endif(${build_doc})
+endif(${BUILD_DOC})
--- QML_Project_Tests/ThirdParty/Doxygen.cmake
+++ QML_Project_Tests/ThirdParty/Doxygen.cmake
@@ -1,20 +1,20 @@
-set(Build_Doc ${Doc_Sub_Target_Name}_BUILD_DOC)
-option(${Build_Doc} "Build documentation (${Doc_Sub_Target_Name})" OFF)
+set(BUILD_DOC ${DOC_OPTION_NAME}_BUILD_DOC)
+option(${BUILD_DOC} "Build documentation (${DOC_OPTION_NAME})" OFF)
 
-if (${Build_Doc})
+if (${BUILD_DOC})
 	find_package(Doxygen)
 
 	if (DOXYGEN_FOUND)
 		# set input and output files
-		set(DOXYGEN_IN ${CMAKE_SOURCE_DIR}/Doxyfile.in)
+		set(DOXYGEN_IN ${CMAKE_SOURCE_DIR}/Configs/Doxyfile.in)
 		set(DOXYGEN_OUT ${CMAKE_CURRENT_SOURCE_DIR}/Docs/Doxyfile)
 
 		# request to configure the file
 		configure_file(${DOXYGEN_IN} ${DOXYGEN_OUT} @ONLY)
-		message("Doxygen build started for ${Doc_Sub_Target_Name}")
+		message("Doxygen build started for ${DOC_TARGET_NAME}")
 
 		# note the option ALL which allows to build the docs together with the application
-		add_custom_target(Doc_Doxygen_${Doc_Sub_Target_Name} ALL
+		add_custom_target(_run_doxygen_${DOC_TARGET_NAME} ALL
 			COMMAND ${DOXYGEN_EXECUTABLE} ${DOXYGEN_OUT}
 			WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
 			COMMENT "Generating API documentation with Doxygen"
@@ -23,4 +23,4 @@ if (${Build_Doc})
 	  message("Doxygen need to be installed to generate the doxygen documentation")
 	endif(DOXYGEN_FOUND)
 	
-endif(${Build_Doc})
+endif(${BUILD_DOC})
